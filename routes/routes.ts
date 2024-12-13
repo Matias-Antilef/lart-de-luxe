3787291f863da95283dd6794af7e4758153a50e9
Matias-Antilef <matias.romero.dv@.com>
2024-12-13 09:21:58 -0300

    add routes models

---
+++ routes/routes.ts
@@ -0,0 +1,10 @@
+export enum PublicRoutes {
+  CART = "cart",
+  AUTH = "auth",
+  LOGIN = "login",
+  REGISTER = "register",
+  FAVORITES = "favorites",
+  PRODUCT_INFO = "product_info",
+  SEARCH = "search",
+  ACCOUNT = "account",
+}
