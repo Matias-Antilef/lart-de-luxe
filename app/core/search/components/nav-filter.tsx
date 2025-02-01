7475e9420a75908f0744ee652f48c2a974cd80dd
Matías Antilef <matias.romero.dv@gmail.com>
2025-02-01 16:25:19 -0300

    search form

---
+++ app/core/search/components/nav-filter.tsx
@@ -0,0 +1,131 @@
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Slider } from "@/components/ui/slider";
+import { SlidersHorizontal } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+interface FilterForm {
+  priceMin?: number;
+  priceMax?: number;
+  category?: string;
+}
+
+function NavFilter({ handleFilters }: { handleFilters: any }) {
+  const [priceMin, setPriceMin] = useState<number>(0);
+  const [priceMax, setPriceMax] = useState<number>(3000);
+  const { register, handleSubmit, setValue, watch } = useForm<FilterForm>();
+
+  function onSubmit(data: FilterForm) {
+    const filteredData = Object.fromEntries(
+      Object.entries(data).filter(
+        ([_, value]) => value !== undefined && value !== ""
+      )
+    );
+
+    handleFilters(filteredData);
+  }
+
+  return (
+    <Sheet>
+      <SheetTrigger className=" flex gap-2 border-[1px] rounded-xl p-2 px-3 border-black ">
+        <SlidersHorizontal />
+        Agregar filtros
+      </SheetTrigger>
+      <SheetContent className="mt-[55px]">
+        <SheetHeader>
+          <SheetTitle>Filtrar productos</SheetTitle>
+          <SheetDescription>
+            <form
+              className="flex flex-col gap-10"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <section className="flex flex-col gap-2">
+                <div className="flex items-center gap-2 ">
+                  <span>Precio mínimo:</span>
+                  <input
+                    {...register("priceMin")}
+                    type="number"
+                    min={0}
+                    max={3000}
+                    value={priceMin}
+                    onChange={(e) => setPriceMin(Number(e.target.value))}
+                    className="border p-1 w-[80px]"
+                  />
+                </div>
+                <Slider
+                  defaultValue={[priceMin]}
+                  max={3000}
+                  step={10}
+                  className="max-w-[200px]"
+                  value={[priceMin]}
+                  onValueChange={(value) => setPriceMin(value[0])}
+                />
+              </section>
+
+              <section className="flex flex-col gap-2">
+                <div className="flex items-center gap-2">
+                  <span>Precio máximo</span>
+                  <input
+                    {...register("priceMax")}
+                    type="number"
+                    min={0}
+                    max={3000}
+                    value={priceMax}
+                    onChange={(e) => setPriceMax(Number(e.target.value))}
+                    className="border p-1 w-[80px]"
+                  />
+                </div>
+                <Slider
+                  max={3000}
+                  step={10}
+                  className="max-w-[200px]"
+                  value={[priceMax]}
+                  onValueChange={(value) => setPriceMax(value[0])}
+                />
+              </section>
+
+              <Select
+                onValueChange={(value) => setValue("category", value)}
+                value={watch("category") || ""}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Filtrar categoría" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Todo</SelectLabel>
+                    <SelectItem value="bolsas">Bolsas</SelectItem>
+                    <SelectItem value="remeras">Remeras</SelectItem>
+                    <SelectItem value="zapatos">Zapatos</SelectItem>
+                    <SelectItem value="polerones">Polerones</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Button type="submit">Aplicar filtros</Button>
+            </form>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default NavFilter;
--- app/core/search/page.tsx
+++ app/core/search/page.tsx
@@ -13,18 +13,41 @@ import { Input } from "@/components/ui/input";
 import { FetchProducts, ProductCardModel } from "@/models/product.model";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import NavFilter from "./components/nav-filter";
 
 function SearchPage() {
   const [products, setProducts] = useState<FetchProducts>({ products: [] });
-  const [search, setSearch] = useState("");
+  const [filterName, setFilterName] = useState("");
+  const [filtersAdvanced, setFiltersAdvanced] = useState<{}>({});
+
+  // page=1&size=10&name=a&minPrice=2&maxPrice=3&category=a&orderByHPrice=true&orderByLPrice=true&orderByNameAz=true&orderByNameZa=true
   useEffect(() => {
     axios
-      // .get(process.env.NEXT_PUBLIC_BASE_URL + "/products/getall")
+      // .get(process.env.NEXT_PUBLIC_BASE_URL + "/products/filter?")
       .get("/products.json")
       .then((res) => {
         setProducts(res.data);
       });
   }, []);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (filterName.trim().length === 0) {
+        alert("Please enter a value");
+      } else {
+        alert(`Input enviado: ${filterName}`);
+        setFilterName("");
+      }
+    }
+  };
+
+  const handleFilters = (filters: any) => {
+    setFiltersAdvanced(filters);
+  };
+
+  useEffect(() => {
+    alert(JSON.stringify(filtersAdvanced));
+  }, [filtersAdvanced]);
   return (
     <div>
       <Card>
@@ -32,9 +55,15 @@ function SearchPage() {
           <CardDescription className="px-52 py-3">
             <Input
               placeholder="Filter your product"
-              className="py-6 px-10 rounded-full  border-black"
+              className="py-6 px-10 rounded-full border-neutral-800 text-black"
+              value={filterName}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setFilterName(e.target.value)}
             />
           </CardDescription>
+          <div className="items-center justify-center flex">
+            <NavFilter handleFilters={handleFilters} />
+          </div>
         </CardHeader>
         <CardContent>
           <ProductsWrapper className="grid-cols-4 gap-5 max-xl:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
@@ -55,7 +84,7 @@ function SearchPage() {
                     categories={categories}
                     principalPic={principalPic}
                     name={name}
-                    className="h-[35vh] min-h-[300px] max-lg:h-[35vh] max-sm:h-[50vh]"
+                    className=" min-h-[200px] max-lg:h-[35vh] max-sm:h-[50vh]"
                   />
                 )
               )}
